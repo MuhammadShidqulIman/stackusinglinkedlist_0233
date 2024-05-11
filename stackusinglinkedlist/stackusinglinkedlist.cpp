ab937a3a12a7a0213620d5c9f40ae39fa3224df3
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-11 12:09:06 +0700

    Implementasi metode pop untuk menghapus elemen dari stack

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -31,6 +31,12 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        }
+    }
 };
 
 int main()
