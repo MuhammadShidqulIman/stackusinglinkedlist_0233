346fc95589e8abc175cbd16ab860cac544fe4f21
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-05-11 12:00:09 +0700

    Perbarui pointer top untuk menunjuk ke newNode dan cetak nilai yang dipush

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -27,6 +27,9 @@ public:
         Node* newNode = new Node();
         newNode->data = value;
         newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
     }
 };
 
